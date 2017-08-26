f3bef2c2402de5c8f466a8a09085d17923776d6d
Godfrey Best <gcb246@stern.nyu.edu>
2017-08-26 18:26:30 -0400

    build

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+// require('./routes/authRoutes');
+
+if (process.env.NODE_ENV === 'production') {
+    // Make sure Express will serve production assets like main.js or main.css
+    // if it doesn't find the route in the defined routes above
+    app.use(express.static('client/build'));
+
+    // Express will serve up index.html if it doesn't recognize the route
+    const path = require('path');
+    // this is the last resort here if the route cannot be found above
+    app.get('*', (req, res ) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
